7ef18cc05b58d6c8743bf20b1bae91597ca786e3
Peter Skopal <peter.skopal@hotmail.com>
2017-09-13 15:07:18 +0200

    feat(handshake): making complete handshake with server and client

--- src/Client.java
+++ src/Client.java
@@ -5,16 +5,18 @@ import java.security.KeyPair;
 
 public class Client {
 
-	public static void main(String args[]) {
-		DatagramSocket sock = null;
-		int port = 7777;
+	public static void main(String args[]) throws InterruptedException {
+		DatagramSocket recSock = null;
+		DatagramSocket sendSock = null;
+		int recPort = 7778;
+		int runPort = 7777;
 		String s;
 		String serverPublicKey;
 		DiffieHellman difHel = new DiffieHellman();
 		
 		KeyPair keyPair = difHel.generateKeys();
-		System.out.println(keyPair.getPrivate());
-		System.out.println(keyPair.getPublic());
+//		System.out.println(keyPair.getPrivate());
+//		System.out.println(keyPair.getPublic());
 		
 		// client incoming messages
 		BufferedReader cin = new BufferedReader(new InputStreamReader(System.in));
@@ -24,34 +26,38 @@ public class Client {
 		DatagramPacket incoming = new DatagramPacket(buffer, buffer.length);
 
 		try {
-			sock = new DatagramSocket();
+			sendSock = new DatagramSocket();
+			recSock = new DatagramSocket(recPort);
 			InetAddress host = InetAddress.getByName("localhost");
+			System.out.println("Client running on: " + host + ":" + runPort);
+			System.out.println("Client listening on: " + host + ":" + recPort);
+			System.out.println("Sending Handshake to Server");
 
-			byte[] handshake = "Hello Server Handshake".getBytes();
-			System.out.println("Making Handshake with message: '" + handshake.toString() + "'");
-			sock.send(new DatagramPacket(handshake, handshake.length, host, port));
+			byte[] handshake = ("Hello Server Handshake").getBytes();
+			sendSock.send(new DatagramPacket(handshake, handshake.length, host, runPort));
 
 			while (true) {
-				sock.receive(incoming);
+				recSock.receive(incoming);
 				byte[] data = incoming.getData();
 				String str = new String(data, 0, incoming.getLength());
-				if (str.equals("Client Handshake")) {
-					System.out.println("Server says: 'Client Handshake'");
-					byte[] publicKey = (byte[]) keyPair.getPublic().toString().getBytes();
-					System.out.println("Sending Public Key to Server: " + publicKey);
-					sock.send(new DatagramPacket(publicKey, publicKey.length, host, port));
+				if (str.equals("Hello Client Handshake")) {
+					System.out.println("Server says: " + str);
+					System.out.println("Sending Public Key to Server.");
+					
+					byte[] publicKey = (byte[]) ("Client Public Key" + keyPair.getPublic().toString()).getBytes();
+					sendSock.send(new DatagramPacket(publicKey, publicKey.length, host, runPort));
 					break;
 				}
 			}
-			
+	
 			while (true) {
-				sock.receive(incoming);
+				recSock.receive(incoming);
 				byte[] data = incoming.getData();
 				String str = new String(data, 0, incoming.getLength());
 				if (str.startsWith("Server Public Key")) {
 					serverPublicKey = str.substring("Server Public Key".length(), str.length());
+					
 					System.out.println("Recieving Public Key from Server: " + serverPublicKey);
-					System.out.println(serverPublicKey);
 					break;
 				}
 			}
@@ -62,8 +68,8 @@ public class Client {
 				s = (String) cin.readLine();
 				byte[] b = s.getBytes();
 
-				DatagramPacket dp = new DatagramPacket(b, b.length, host, port);
-				sock.send(dp);
+				DatagramPacket dp = new DatagramPacket(b, b.length, host, runPort);
+				sendSock.send(dp);
 			}
 		} catch (IOException e) {
 			System.err.println("IOException " + e);
--- src/Server.java
+++ src/Server.java
@@ -4,45 +4,56 @@ import java.net.*;
 import java.security.KeyPair;
 
 public class Server {
-			
+
 	public static void main(String args[]) {
-        DatagramSocket sock = null;
-        int port = 7777;
-        
-        String serverPublicKey;
+		DatagramSocket sendSock = null;
+		DatagramSocket recSock = null;
+		int recPort = 7777;
+		int runPort = 7778;
+		String clientPublicKey;
+
 		DiffieHellman difHel = new DiffieHellman();
-        
-		KeyPair keyPair = difHel.generateKeys();
-		System.out.println(keyPair.getPrivate());
-		System.out.println(keyPair.getPublic());
 		
-        try {
-        	sock = new DatagramSocket(port);
-        	byte[] buffer = new byte[65536];
-            DatagramPacket incoming = new DatagramPacket(buffer, buffer.length);
-            System.out.println("Server socket created. Waiting for incoming data...");
-            
-            sock = new DatagramSocket();
+		KeyPair keyPair = difHel.generateKeys();
+		
+		try {
+			sendSock = new DatagramSocket();
 			InetAddress host = InetAddress.getByName("localhost");
-      
-            while(true) {
-                sock.receive(incoming);
-                byte[] data = incoming.getData();
-                String s = new String(data, 0, incoming.getLength());
-                System.out.println(incoming.getAddress().getHostAddress() + " : " + incoming.getPort() + " - " + s);
-                
-                if (s.equals("Hello Server Handshake")) {
-                	byte[] handshake = "Client Handshake".getBytes();
-        			sock.send(new DatagramPacket(handshake, handshake.length, host, port));
-                } else if (s.startsWith("Client Public Key")) {
-                	byte[] publicKey = (byte[]) keyPair.getPublic().toString().getBytes();
-                	sock.send(new DatagramPacket(publicKey, publicKey.length, host, port));
-                }
-                
-            }
-        
-        } catch(IOException e) {
-            System.err.println("IOException " + e);
-        }
-    }
+			recSock = new DatagramSocket(recPort);
+			byte[] buffer = new byte[65536];
+			byte[] helloCl = "Hello Client Handshake".getBytes();
+			byte[] publicKey = (byte[]) ("Server Public Key" + keyPair.getPublic().toString()).getBytes();
+			DatagramPacket incoming = new DatagramPacket(buffer, buffer.length);
+			DatagramPacket helloClient = new DatagramPacket(helloCl, helloCl.length, host, runPort);
+			DatagramPacket serverPublicKey = new DatagramPacket(publicKey, publicKey.length, host, runPort);
+			
+			System.out.println("Server listening on: " + host.toString() + ":" + recPort);
+			System.out.println("Server running on: " + host.toString() + ":" + runPort);
+			System.out.println("Server socket created. Waiting for incoming data...");
+
+			while (true) {
+				recSock.receive(incoming);
+				byte[] data = incoming.getData();
+				
+				String s = new String(data, 0, incoming.getLength());
+				if (s.equals("Hello Server Handshake")) {
+					System.out.println(incoming.getAddress().getHostAddress() + " : " + incoming.getPort() + " - " + s + "\tSUCCESS");
+					
+					sendSock.send(helloClient);
+				} else if (s.startsWith("Client Public Key")) {
+					clientPublicKey = s.substring("Client Public Key".length(), s.length());
+					
+					System.out.println("Recieving Public Key from Client.");
+					System.out.println("Sending Public Key to Client.");
+					
+					sendSock.send(serverPublicKey);
+				} else {			
+					System.out.println(incoming.getAddress().getHostAddress() + " : " + incoming.getPort() + " - " + s);
+				}
+			}
+
+		} catch (IOException e) {
+			System.err.println("IOException " + e);
+		}
+	}
 }
